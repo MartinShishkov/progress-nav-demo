5368b6001e9491c82ffd831879f241471c303f65
MartinShishkov <martin@codeinsiders.net>
2020-09-10 11:56:14 +0300

    first draft

---
+++ src/Progress.NavDemo/NavDemo.Web/Models/ProgressMenuItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NavDemo.Web.Models
+{
+    public class ProgressMenuItem
+    {
+        public ProgressMenuItem(int id, string text, Action<int> onSelect, ProgressMenuItem[] children = null)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(text));
+            Children = children ?? new ProgressMenuItem[0];
+            Id = id;
+            Text = text;
+            OnSelect = onSelect ?? throw new ArgumentNullException(nameof(onSelect));
+        }
+
+        public ProgressMenuItem[] Children { get; }
+
+        public int Id { get; }
+
+        public string Text { get; }
+
+        public Action<int> OnSelect { get; }
+
+        public bool HasChildren => Children.Length > 0;
+    }
+}
